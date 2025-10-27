286af47cac31873d176413bec419135c0e333b8d
Drakoxw <developer17@sinergiaonline.com>
2025-10-26 23:22:38 -0500

    feat: the 'Me' module is created with personal information

--- src/app/modules/me/ui/me-index/me-index.component.ts
+++ src/app/modules/me/ui/me-index/me-index.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-me-index',
   templateUrl: './me-index.component.html',
-  styleUrls: ['./me-index.component.scss']
 })
-export default class MeIndexComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export default class MeIndexComponent {
+  readonly linkWhastapp = 'https://wa.me/573108018388';
+  readonly linkLinkedin = 'https://www.linkedin.com/in/wilmar-roncancio-mendez-b344761bb/';
+  readonly linkGithub = 'https://github.com/Drakoxw';
 }
