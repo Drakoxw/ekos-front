44ff02dbcb1be80bec88b87a2093711fad7eb848
Drakoxw <developer17@sinergiaonline.com>
2025-10-26 23:20:22 -0500

    feat: mocks are added

---
+++ src/app/core/mocks/common.ts
@@ -0,0 +1,3 @@
+import { Pagination } from "@interfaces/index";
+
+export const MOCK_PAGINATION: Pagination = { page: 1, pageSize: 5, search: '' };
---
+++ src/app/core/mocks/product.ts
@@ -0,0 +1,12 @@
+import { ProductData } from "@interfaces/index";
+
+export const MOCK_PRODUCT: ProductData = {
+  id: 7,
+  name: "Trapero",
+  description: "Color Azul",
+  price: 10,
+  stock: 20,
+  category: "Hogar",
+  isAvailable: true,
+  createdAt: "2025-10-26T20:27:36.6655996Z"
+}
